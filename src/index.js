21814156517f2fd149c7c031c4d3a948dcf7fec5
rzadrbi <rzadrbi@gmail.com>
2024-07-29 13:54:22 +0330

    components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class Hello extends React.Component {
+  render(){
+    return(
+      <h1>Time is :</h1>
+    )
+  }
+}
+
+
+class Timer extends React.Component {
+  render(){
+    return(
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    )
+  }
+}
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Hello/>
+        <Timer/>
+      </div>
+    )
+  }
+}
+
+
+// const Timer = ()=>{
+  // const elem = (
+  // <div>
+  {/*  */}
+  {/*  */}
+  {/* </div> */}
+  // );
+  // root.render(elem);
+// }
+const render_instance = ()=>{
+  root.render(<App/>);
+}
+
+setInterval(()=>{
+  render_instance();
+}, 1000)
+
+// createRoot(document.getElementById('root'), render(elem));
+
+
